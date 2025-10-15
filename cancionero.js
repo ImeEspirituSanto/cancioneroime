04213818a5326fe5b1f05e7d4bcc05fe4d2d79de
ImeEspirituSanto <cancionero.ime@gmail.com>
2025-10-15 00:46:21 -0300

    Update cancionero.js

--- cancionero.js
+++ cancionero.js
@@ -1,20 +1,3 @@
-let lastScroll = 0;
-const nav = document.querySelector("nav.navbar");
-
-window.addEventListener("scroll", () => {
-  const currentScroll = window.pageYOffset;
-  
-  if (currentScroll > lastScroll) {
-    // Scroll hacia abajo → ocultar nav
-    nav.style.top = "-80px"; // ajustar según altura del nav
-  } else {
-    // Scroll hacia arriba → mostrar nav
-    nav.style.top = "0";
-  }
-  
-  lastScroll = currentScroll;
-});
-
 document.getElementById("searchBar").addEventListener("input", function () {
   let query = this.value.toLowerCase();
   let songs = document.querySelectorAll(".song");
